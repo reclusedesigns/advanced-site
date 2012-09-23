efba53e7ff56e133bea86c49346e3044e9bee71f
recluse <andrew@reclusedesigns.com.au>
2012-09-23 22:16:12 +1000

    did the header yo! php like a bawse

---
+++ gallery.php
@@ -0,0 +1,89 @@
+<?php include 'header.php'; ?>
+
+	<section class="intro">
+		<div class="zen">
+			<div class="container">
+				<div class="stage_content">
+					<h1>Our Projects</h1>
+					<img src="design/images/body/projects/stage_house.png">
+				</div>
+			</div> <!-- container -->
+		</div>
+		<div class="table_backer">
+		</div>
+	</section>
+	
+	<div class="container">
+		<section class="body projects">
+			<div class="row" id="gallery">
+				<div class="span4">
+					<div class="cover">
+						<a href="gallery-1.html">
+							<img src="design/images/gallery/cover/1.jpg" alt="" />
+							<span>
+								73 Something Street
+							</span>
+						</a>
+					</div> 
+				</div>
+				<div class="span4">
+					<div class="cover">
+						<a href="gallery-1.html">
+							<img src="design/images/gallery/cover/1.jpg" alt="" />
+							<span>
+								73 Something Street
+							</span>
+						</a>
+					</div> 
+				</div>
+				<div class="span4">
+					<div class="cover">
+						<a href="gallery-1.html">
+							<img src="design/images/gallery/cover/1.jpg" alt="" />
+							<span>
+								73 Something Street
+							</span>
+						</a>
+					</div> 
+				</div>
+				<div class="span4">
+					<div class="cover">
+						<a href="gallery-1.html">
+							<img src="design/images/gallery/cover/1.jpg" alt="" />
+							<span>
+								73 Something Street
+							</span>
+						</a>
+					</div> 
+				</div>
+				<div class="span4">
+					<div class="cover">
+						<a href="gallery-1.html">
+							<img src="design/images/gallery/cover/1.jpg" alt="" />
+							<span>
+								73 Something Street
+							</span>
+						</a>
+					</div> 
+				</div>
+				<div class="span4">
+					<div class="cover">
+						<a href="gallery-1.html">
+							<img src="design/images/gallery/cover/1.jpg" alt="" />
+							<span>
+								73 Something Street
+							</span>
+						</a>
+					</div> 
+				</div>
+			</div> <!-- row -->
+		</section>
+	</div>
+	
+	<div class="container">
+		<section class="from_here well">
+			<h4>Like what you see? <strong>Let's talk - (03) 9379 9935</strong></h4>
+		</section>
+	</div>
+
+<?php include 'footer.php'; ?>

57e9ae499a03e5d0919c2de420db9ae78d0bfee1
recluse <andrew@reclusedesigns.com.au>
2012-09-23 21:56:05 +1000

    header sort-of working

--- projects.php
+++ projects.php
@@ -1,82 +1,5 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-	<title>Advanced Property Construction&#8482;</title>
-	<meta name="author" content="Recluse">
-	
-	<!-- muay styles -->
-	<link rel="stylesheet" href="design/vendor/bootstrap/css/bootstrap.css" type="text/css" media="screen" charset="utf-8">
-	<link rel="stylesheet" href="design/css/layout.css" type="text/css" media="screen" charset="utf-8">
-	<link rel="stylesheet" href="design/vendor/nivo-slider/nivo-slider.css" type="text/css" media="screen" />
-	
-	<!-- the javascriptage -->
-	<script src="design/vendor/jquery.js" type="text/javascript" charset="utf-8"></script>
-	<script src="design/vendor/bootstrap/js/bootstrap.js" type="text/javascript" charset="utf-8"></script>
-	<script src="design/vendor/scrollto.js" type="text/javascript" charset="utf-8"></script>
-	<script src="design/vendor/nivo-slider/jquery.nivo.slider.js" type="text/javascript" charset="utf-8"></script>
-	<script src="design/scripts/toys.js" type="text/javascript" charset="utf-8"></script>
-	
-	<!-- typekit! -->
-	
-</head>
-<body>
+<?php include 'header.php'; ?>
 
-	<div class="body_push"></div>
-
-	<div class="navbar navbar-fixed-top">
-    <div class="navbar-inner">
-      <div class="container">
-        <a class="brand" href="index.html">Advanced Property Construction</a>
-				<ul class="nav pull-right">
-					<li><img src="design/images/header/builder_badges.png"></li>
-					<li><a href="#" class="first">&nbsp;</a></li>
-					<li><a href="process.html" class="process">Process</a></li>
-					<li><a href="projects.html" class="projects">Projects</a></li>
-					<li><a href="#" class="contact js_contact_click">Contact</a></li>
-					<li><a href="#" class="last">&nbsp;</a></li>
-				</ul>
-      </div>
-    </div><!-- /navbar-inner -->
-  </div> <!-- header -->
-	<div class="contact_drop js_contact_toggle">	
-		<div class="container">
-			<div class="row">
-				<div class="span4">
-					<h4><span>Talk to a real person by calling </span> (03) 9379 9935</h4>
-					<p>Or send us an email by filling out the form over here</p>
-				</div>
-				<div class="offset1 span4">
-					<form class="form-horizontal">
-        <fieldset>
-					<div class="control-group">
-            <label class="control-label" for="name">Your Name</label>
-            <div class="controls">
-              <input type="text" class="input-xlarge" id="name">
-            </div>
-          </div>
-          <div class="control-group">
-            <label class="control-label" for="contact_details">Email Address</label>
-            <div class="controls">
-              <input type="text" class="input-xlarge" id="contact_details">
-            </div>
-          </div>
-          <div class="control-group">
-            <label class="control-label" for="textarea">Your message</label>
-            <div class="controls">
-              <textarea class="input-xlarge" id="textarea" rows="3"></textarea>
-            </div>
-          </div>
-          <div class="form-actions">
-            <input type="submit" class="btn btn-primary" value="Send"></button>
-          </div>
-        </fieldset>
-      </form>
-				</div>
-			</div>
-		</div>
-	</div> <!-- contact_drop -->
-	
 	<section class="intro">
 		<div class="zen">
 			<div class="container">
@@ -163,13 +86,4 @@
 		</section>
 	</div>
 	
-	<footer>
-		<div class="container">
-			<a class="facebook" href="http://www.facebook.com/advancedpropertyconstruction">Follow us on Facebook</a>
-			<p class="copyright">Copyright Advanced Property Construction 2012 - All Rights Reserved</p>
-			<a class="recluse" href="http://www.reclusedesigns.com.au">Made by <span>Recluse</span></a>
-		</div>
-	</footer>
-
-</body>
-</html>
+<?php include 'footer.php'; ?>

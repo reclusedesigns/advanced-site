f52813f64fb62140ba2e457e343c49584163fb25
recluse <andrew@reclusedesigns.com.au>
2012-10-31 23:17:06 +1100

    adding three properties

---
+++ pheonix.php
@@ -0,0 +1,138 @@
+<?php include 'header.php'; ?>
+
+	<section class="intro">
+		<div class="zen zen_gallery">
+			<div class="container">
+				<img src="design/images/gallery/pheonix/main.jpg" alt="main" width="956" height="340" />
+			</div> <!-- container -->
+		</div>
+		<div class="table_backer gallery_shadow">
+			<img src="design/images/carousel/shadow_large.png">
+		</div>
+	</section>
+	
+	<div class="container">
+		<div id="gallery" data-toggle="modal-gallery" data-target="#modal-gallery">
+			<div class="row">
+			
+				<div class="span2">
+					<a href="design/images/gallery/pheonix/normal/1.jpg" rel="gallery"><img src="design/images/gallery/pheonix/thumbs/1.jpg" alt="1" width="140" height="140" /></a>
+				</div>
+				<div class="span2">
+					<a href="design/images/gallery/pheonix/normal/2.jpg" rel="gallery"><img src="design/images/gallery/pheonix/thumbs/2.jpg" alt="1" width="140" height="140" /></a>
+				</div>
+				<div class="span2">
+					<a href="design/images/gallery/pheonix/normal/3.jpg" rel="gallery"><img src="design/images/gallery/pheonix/thumbs/3.jpg" alt="1" width="140" height="140" /></a>
+				</div>
+				<div class="span2">
+					<a href="design/images/gallery/pheonix/normal/4.jpg" rel="gallery"><img src="design/images/gallery/pheonix/thumbs/4.jpg" alt="1" width="140" height="140" /></a>
+				</div>
+				<div class="span2">
+					<a href="design/images/gallery/pheonix/normal/5.jpg" rel="gallery"><img src="design/images/gallery/pheonix/thumbs/5.jpg" alt="1" width="140" height="140" /></a>
+				</div>
+				<div class="span2">
+					<a href="design/images/gallery/pheonix/normal/6.jpg" rel="gallery"><img src="design/images/gallery/pheonix/thumbs/6.jpg" alt="1" width="140" height="140" /></a>
+				</div>
+				
+			</div>
+			
+			<div class="row">
+			
+				<div class="span2">
+					<a href="design/images/gallery/pheonix/normal/7.jpg" rel="gallery"><img src="design/images/gallery/pheonix/thumbs/7.jpg" alt="1" width="140" height="140" /></a>
+				</div>
+				<div class="span2">
+					<a href="design/images/gallery/pheonix/normal/8.jpg" rel="gallery"><img src="design/images/gallery/pheonix/thumbs/8.jpg" alt="1" width="140" height="140" /></a>
+				</div>
+				<div class="span2">
+					<a href="design/images/gallery/pheonix/normal/9.jpg" rel="gallery"><img src="design/images/gallery/pheonix/thumbs/9.jpg" alt="1" width="140" height="140" /></a>
+				</div>
+				<div class="span2">
+					<a href="design/images/gallery/pheonix/normal/10.jpg" rel="gallery"><img src="design/images/gallery/pheonix/thumbs/10.jpg" alt="1" width="140" height="140" /></a>
+				</div>
+				<div class="span2">
+					<a href="design/images/gallery/pheonix/normal/11.jpg" rel="gallery"><img src="design/images/gallery/pheonix/thumbs/11.jpg" alt="1" width="140" height="140" /></a>
+				</div>
+				<div class="span2">
+					<a href="design/images/gallery/pheonix/normal/12.jpg" rel="gallery"><img src="design/images/gallery/pheonix/thumbs/12.jpg" alt="1" width="140" height="140" /></a>
+				</div>
+				
+			</div>
+			
+			<div class="row">
+			
+				<div class="span2">
+					<a href="design/images/gallery/pheonix/normal/13.jpg" rel="gallery"><img src="design/images/gallery/pheonix/thumbs/13.jpg" alt="1" width="140" height="140" /></a>
+				</div>
+				<div class="span2">
+					<a href="design/images/gallery/pheonix/normal/14.jpg" rel="gallery"><img src="design/images/gallery/pheonix/thumbs/14.jpg" alt="1" width="140" height="140" /></a>
+				</div>
+				<div class="span2">
+					<a href="design/images/gallery/pheonix/normal/15.jpg" rel="gallery"><img src="design/images/gallery/pheonix/thumbs/15.jpg" alt="1" width="140" height="140" /></a>
+				</div>
+				<div class="span2">
+					<a href="design/images/gallery/pheonix/normal/16.jpg" rel="gallery"><img src="design/images/gallery/pheonix/thumbs/16.jpg" alt="1" width="140" height="140" /></a>
+				</div>
+				<div class="span2">
+					<a href="design/images/gallery/pheonix/normal/17.jpg" rel="gallery"><img src="design/images/gallery/pheonix/thumbs/17.jpg" alt="1" width="140" height="140" /></a>
+				</div>
+				<div class="span2">
+					<a href="design/images/gallery/pheonix/normal/18.jpg" rel="gallery"><img src="design/images/gallery/pheonix/thumbs/18.jpg" alt="1" width="140" height="140" /></a>
+				</div>
+				
+			</div>
+
+
+			<div class="row">
+			
+				<div class="span2">
+					<a href="design/images/gallery/pheonix/normal/19.jpg" rel="gallery"><img src="design/images/gallery/pheonix/thumbs/19.jpg" alt="1" width="140" height="140" /></a>
+				</div>
+				<div class="span2">
+					<a href="design/images/gallery/pheonix/normal/20.jpg" rel="gallery"><img src="design/images/gallery/pheonix/thumbs/20.jpg" alt="1" width="140" height="140" /></a>
+				</div>
+				<div class="span2">
+					<a href="design/images/gallery/pheonix/normal/21.jpg" rel="gallery"><img src="design/images/gallery/pheonix/thumbs/21.jpg" alt="1" width="140" height="140" /></a>
+				</div>
+				<div class="span2">
+					<a href="design/images/gallery/pheonix/normal/22.jpg" rel="gallery"><img src="design/images/gallery/pheonix/thumbs/22.jpg" alt="1" width="140" height="140" /></a>
+				</div>
+				<div class="span2">
+					<a href="design/images/gallery/pheonix/normal/23.jpg" rel="gallery"><img src="design/images/gallery/pheonix/thumbs/23.jpg" alt="1" width="140" height="140" /></a>
+				</div>
+				<div class="span2">
+					<a href="design/images/gallery/pheonix/normal/24.jpg" rel="gallery"><img src="design/images/gallery/pheonix/thumbs/24.jpg" alt="1" width="140" height="140" /></a>
+				</div>
+				
+			</div>
+			
+			
+			<div class="row">
+			
+				<div class="span2">
+					<a href="design/images/gallery/pheonix/normal/25.jpg" rel="gallery"><img src="design/images/gallery/pheonix/thumbs/25.jpg" alt="1" width="140" height="140" /></a>
+				</div>
+				<div class="span2">
+					<a href="design/images/gallery/pheonix/normal/26.jpg" rel="gallery"><img src="design/images/gallery/pheonix/thumbs/26.jpg" alt="1" width="140" height="140" /></a>
+				</div>
+				<div class="span2">
+					<a href="design/images/gallery/pheonix/normal/27.jpg" rel="gallery"><img src="design/images/gallery/pheonix/thumbs/27.jpg" alt="1" width="140" height="140" /></a>
+				</div>
+				<div class="span2">
+					<a href="design/images/gallery/pheonix/normal/28.jpg" rel="gallery"><img src="design/images/gallery/pheonix/thumbs/28.jpg" alt="1" width="140" height="140" /></a>
+				</div>
+
+				
+			</div>
+
+		</div>
+	</div>
+	
+	
+	<div class="container">
+		<section class="from_here well">
+			<h4>Give us a call on <strong>(03) 9379 9935</strong></h4>
+		</section>
+	</div>
+
+<?php include 'footer.php'; ?>
